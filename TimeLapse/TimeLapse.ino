a9a8dfeb092e5abe454505ab54ef2329de1143d4
pipezas <pipezas@gmail.com>
2021-03-13 13:53:33 +0200

    Update TimeLapse.ino

--- TimeLapse/TimeLapse.ino
+++ TimeLapse/TimeLapse.ino
@@ -3,38 +3,44 @@
 #include "camera.h"
 #include "lapse.h"
 
-const char *ssid = "...";
-const char *password = "...";
+const char *ssid = "ESP32-CAM Access Point";
+const char *password = "12345678";
 
 void startCameraServer();
 
 void setup()
 {
-	Serial.begin(115200);
-	Serial.setDebugOutput(true);
-	Serial.println();
-	initFileSystem();
-	initCamera();
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+  initFileSystem();
+  initCamera();
+/*
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected
+*/
 
-	WiFi.begin(ssid, password);
-	while (WiFi.status() != WL_CONNECTED)
-	{
-		delay(500);
-		Serial.print(".");
-	}
-	Serial.println("");
-	Serial.println("WiFi connected");
-	startCameraServer();
-	Serial.print("Camera Ready! Use 'http://");
-	Serial.print(WiFi.localIP());
-	Serial.println("' to connect");
+WiFi.softAP(ssid, password);
+
+  startCameraServer();
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+  pinMode(4, OUTPUT);
+  digitalWrite(4, 0); 
 }
 
 void loop()
 {
-	unsigned long t = millis();
-	static unsigned long ot = 0;
-	unsigned long dt = t - ot;
-	ot = t;
-	processLapse(dt);
+  unsigned long t = millis();
+  static unsigned long ot = 0;
+  unsigned long dt = t - ot;
+  ot = t;
+  processLapse(dt);
 }
